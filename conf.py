6cc986f4035fe24bd09ec8e42d86d0e02b0290e5
Edro6728 <100054872+Edro6728@users.noreply.github.com>
2022-05-25 10:52:49 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-api_hash = "apihash"
-api_id = 1736372819
-bot_token = "bottoken"
-authorized_users = []
-admin_user = 'admindelbot'
+api_hash = "2c0b01283b1b67f7087a92cc81cd91f8"
+api_id = 18403498
+bot_token = "5351314676:AAH_ZbSODC5l6ak9pf7dcCaqWW8ZhQTRpS8"
+authorized_users = [916354580]
+admin_user = 'Edump2101'
